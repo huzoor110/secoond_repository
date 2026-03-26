e918c1876552048c88306adcbb9c083bad028b2d
Huzoor_Laghari <huzoorbuxlsghari@gmail.com>
2026-03-26 19:46:11 +0500

    added this file

---
+++ python.py
@@ -0,0 +1,10 @@
+name = "huzoor"
+age = 22
+price = 32.5
+
+print("my name is:"  , name)
+print (" my age is:" , age)
+print("the price is: " , price)
+print(type(name))
+print(type(age))
+print(type(price))
